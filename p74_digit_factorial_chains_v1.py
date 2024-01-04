a6358d4e419d35192a3321c21e043b98a63e17cb
Jethack <jethack23@gmail.com>
2024-01-05 06:23:59 +0900

    74

---
+++ p74_digit_factorial_chains_v1.py
@@ -0,0 +1,77 @@
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+digit_factorials = list(map(factorial, range(10)))
+
+
+def chain_length(n):
+    mem = {*()}
+    rst = 0
+    while not n in mem:
+        rst += 1
+        mem.add(n)
+        n = sum(map(lambda x: digit_factorials[int(x)], str(n)))
+    return rst
+
+
+def solution(M):
+    rst = 0
+    for i in range(M):
+        if chain_length(i) == 60:
+            rst += 1
+    return rst
+
+
+from time import time
+
+
+def main():
+    start = time()
+    print(solution(1000000))
+    print("elapsed:", time() - start)
+
+
+main()
+
+
+# translated from below s-expression
+
+# version : 0.1.8
+# (def factorial [n]
+#   (if (< n 2)
+#       (return 1)
+#       (return (* n (factorial (- n 1))))))
+#
+# (= digit-factorials (list (map factorial (range 10))))
+#
+# (def chain-length [n]
+#   (= mem {,})
+#   (= rst 0)
+#   (while (not (in n mem))
+#     (+= rst 1)
+#     (mem.add n)
+#     (= n (sum (map (lambda [x] (sub digit-factorials (int x))) (str n)))))
+#   (return rst))
+#
+# (def solution [M]
+#   (= rst 0)
+#   (for i (range M)
+#     (if (== (chain-length i) 60)
+#         (+= rst 1)))
+#   (return rst))
+#
+# (defmacro euler [*inputs]
+#   (return `(do (from time [time])
+#
+#                (def main []
+#                  (= start (time))
+#                  (print (solution ~@inputs))
+#                  (print "elapsed:" (- (time) start)))
+#
+#                (main))))
+#
+# (euler 1000000)
